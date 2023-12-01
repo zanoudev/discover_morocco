0464f36be7066000a40b455704294b9ff399a625
zanoudev <zanou.dev@gmail.com>
2023-12-01 14:10:02 -0500

    com

--- eat.js
+++ eat.js
@@ -1,5 +1,8 @@
 function hoverImage() {
   document.querySelector(".tajine-image").src = "images/tajine-open.png";
+  // Play the audio
+  var audio = document.getElementById("hoverSound");
+  audio.play();
 }
 
 function unhoverImage() {
