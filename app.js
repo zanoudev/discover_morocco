c17fd06d8078242a1c93b1f504560f1bdd1979b3
zanourih <zanou.rih0@gmail.com>
2023-11-30 23:49:17 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const toggleNav = () => {
+  document.body.dataset.nav =
+    document.body.dataset.nav === "true" ? "false" : "true";
+};
---
+++ walk.js
@@ -0,0 +1,7 @@
+function hoverImage() {
+  document.querySelector(".belgha-image").src = "images/belgha-jaune-no-bg.png";
+}
+
+function unhoverImage() {
+  document.querySelector(".belgha-image").src = "images/belgha-no-bg.png";
+}
