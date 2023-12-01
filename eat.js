ed61228e21ffdfacca9c90ce2c1ad68104c87abc
zanoudev <zanou.dev@gmail.com>
2023-12-01 10:10:10 -0500

    new pages

---
+++ eat.js
@@ -0,0 +1,7 @@
+function hoverImage() {
+  document.querySelector(".tajine-image").src = "images/tajine-open.png";
+}
+
+function unhoverImage() {
+  document.querySelector(".tajine-image").src = "images/tajine-close.png";
+}
